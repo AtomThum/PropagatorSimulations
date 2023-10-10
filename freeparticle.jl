7eee2423c4f47db6dd2987cc8c2b702773368c16
AtomThum <atom.olee@gmail.com>
2023-10-10 19:25:56 +0700

    one freeparticle succeeded

--- freeparticle.jl
+++ freeparticle.jl
@@ -6,51 +6,37 @@ using Plots
 
 # Propagator for the free particle (Let ℏ = 1)
 K(x₀, x, t, m) = sqrt(m/(2*π*im*t)) * exp(-m*(x₀ - x)^2/t) # Scalar number
+P(Ψ) = real.(Ψ)^2 + imag.(Ψ)^2 # Convert to probability distribution
 
 simrange = 5 # Size of simulation (Because the way it's coded, ±simrange will have an infinite potential barrier)
-dx = 0.02
+dx = 0.01
 simlength = 5
 dt = 0.1
 N = round(Int, simrange/(2 * dx)) # Number of x slots
 
-# Initial condition
-posVect = [-(n * dx) for n = -N:N]
-# range(start = -simrange, stop = simrange, length = N)
-initVect = [exp(-(n * dx)^2) for n = -N:N] # Gaussian wave packets
+posVect = [(n * dx) for n in -N:N] # Position basis
+initVect = [exp(-(n * dx)^2 + 1) for n = -N:N] # Initial ket
+plot(posVect, initVect) # Plotting the initial condition
 
-# Probability density for plotting
-probabilityDensity(Ψ) = real(Ψ)^2 + imag(Ψ)^2
-
-# Single point propagator
-function propagatePoint(initVect, posVect, targetPos, targetTime, mass)
-    pdf = 0
+# Propagator sampling
+function propagate(initVect, targetTime, posVect)
     vectSize = size(posVect, 1)
+    propVect = zeros(vectSize)im
     for i in range(1, vectSize)
-        initProb = initVect[i]
-        initPos = posVect[i]
-        pdf =+ initProb * K(initPos, targetPos, targetTime, mass)
+        pdf = 0.0 + 0.0*im
+        for j in range(1, vectSize)
+            pdf = pdf + initVect[j]*K(posVect[j], posVect[i], targetTime, 1)
+        end
+        propVect[i] = pdf
     end
-    return probabilityDensity(pdf)
-end
-
-# Propagating the whole wave function through time
-function propagateVect(initVect, posVect, targetTime, mass)
-    vectSize = size(posVect, 1)
-    propagatedVect = zeros(vectSize)
-    for j in range(1, vectSize)
-        propagatedVect[j] = propagatePoint(
-            initVect, posVect, posVect[j], targetTime, mass
-        )
-    end
-    return propagatedVect
+    return(propVect)
 end
 
 tValues = range(0, simlength, length = round(Int, simlength/dt))
 animation = @animate for t in tValues
     global posVect
     global initVect
-    newVect = normalize(propagateVect(initVect, posVect, t, 1))
-    plot(posVect, [newVect], title = "t = $t")
+    newVect = normalize(P.(propagate(initVect, t, posVect)))
+    plot(posVect, [newVect], title = "t = $t", yrange = (0, 0.25))
 end
-
-gif(animation, "freeparticle.gif", fps = 15)
+gif(animation, "freeparticle.gif", fps = 30)
