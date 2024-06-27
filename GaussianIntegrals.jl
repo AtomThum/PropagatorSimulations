ea58f9235bfa445bec848272bbf02967f8d310a0
AtomThum <atom.olee@gmail.com>
2024-06-27 23:01:22 +0700

    june 27

---
+++ GaussianIntegrals.jl
@@ -0,0 +1,18 @@
+using SymPy
+using SpecialFunctions
+using Latexify
+
+@syms x, y, z
+@syms a::(real, positive), b::(real, positive), c::(real, positive), n::(real, positive)
+expr = x^n * exp(- a*x^2 - b*x - c)
+
+for i in 1:30
+    currentExpr = expr.subs(n, i)
+    currentExprInt = factor(integrate(currentExpr, (x, -oo, oo))).subs(erf(b/(2*sqrt(a))) + erfc(b/(2*sqrt(a))), 1)
+    if i % 2 == 0
+        display(currentExprInt.args[3])
+    else
+        display(currentExprInt.args[4])
+    end
+    display(currentExprInt)
+end
--- README.md
+++
@@ -1,5 +0,0 @@
-# Propagator Simulation
-
-Just a collection of code that I use in the second part of the project, "The dynamics of a quantum entangled systems using the Feynman path integrals method." Note that most of the code here is just me trying to properly evaluate the two particle propagator that has an interaction term as a spring potential. There might also be some simulations, due to my inability to read complex physical equations.
-
-(Yes, I changed the name of the project because the old name was to)
